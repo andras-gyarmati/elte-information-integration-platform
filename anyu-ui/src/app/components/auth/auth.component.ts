98bc02643024b31cf7172b1393dd545ea6160b66
Andras Gyarmati <bandieee16@gmail.com>
2023-11-24 22:19:28 +0100

    add empty auth component and service

---
+++ anyu-ui/src/app/components/auth/auth.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+import {AuthService} from "../../services/auth.service";
+
+@Component({
+  selector: 'app-auth',
+  templateUrl: './auth.component.html',
+  styleUrls: ['./auth.component.sass']
+})
+export class AuthComponent {
+  constructor(private authService: AuthService) {
+  }
+}

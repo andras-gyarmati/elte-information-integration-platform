3972c2e47ca3906501b779c27248bd519d18d510
Andras Gyarmati <bandieee16@gmail.com>
2023-11-25 01:45:49 +0100

    show swagger in prod

--- ANYU.Api/Program.cs
+++ ANYU.Api/Program.cs
@@ -97,11 +97,11 @@ public class Program
         var app = builder.Build();
 
         // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
-        {
+        // if (app.Environment.IsDevelopment())
+        // {
             app.UseSwagger();
             app.UseSwaggerUI();
-        }
+        // }
         using (var scope = app.Services.CreateScope())
         {
             var dbContext = scope.ServiceProvider.GetRequiredService<AnyuDbContext>();

59692f83f612919feb202e7485591eab8e66333e
Andras Gyarmati <bandieee16@gmail.com>
2023-12-16 11:55:56 +0100

    add more details to course page

--- ANYU.Api/Services/UserService.cs
+++ ANYU.Api/Services/UserService.cs
@@ -108,24 +108,75 @@ public class CourseService : IRequestHandler<GetCoursesRequest, PagedListResult<
 
     public async Task<Result<CourseResponse>> Handle(GetCourseRequest request, CancellationToken cancellationToken)
     {
+        // for debug if course or courseinstance or semester tables have no data add some dummy data
+        if (!_context.Courses.Any())
+        {
+            var course = new Course
+            {
+                Name = "Course 1",
+                Code = "C1",
+                Category = "Category 1",
+                Credit = 1,
+                Description = "Description 1",
+                CreatedAt = DateTime.UtcNow,
+                CreatedBy = "System"
+            };
+            _context.Courses.Add(course);
+            await _context.SaveChangesAsync(cancellationToken);
+        }
+        if (!_context.Semesters.Any())
+        {
+            var semester = new Semester
+            {
+                StartDate = DateTime.UtcNow,
+                EndDate = DateTime.UtcNow,
+                StudyPeriodWeeks = 14,
+                ExamPeriodWeeks = 6,
+                CreatedAt = DateTime.UtcNow,
+                CreatedBy = "System"
+            };
+            _context.Semesters.Add(semester);
+            await _context.SaveChangesAsync(cancellationToken);
+        }
+        if (!_context.CourseInstances.Any())
+        {
+            var courseInstance = new CourseInstance
+            {
+                CourseId = 1,
+                SemesterId = 1,
+                Description = "Description 1",
+                CreatedAt = DateTime.UtcNow,
+                CreatedBy = "System"
+            };
+            _context.CourseInstances.Add(courseInstance);
+            await _context.SaveChangesAsync(cancellationToken);
+        }
+        // end debug, remove if not needed
+
         try
         {
-            var course = await _context.Courses
+            var courseInstance = await _context.CourseInstances
+                .Include(x => x.Course)
+                .Include(x => x.Semester)
+                .OrderBy(x => x.Semester.StartDate)
                 .AsQueryable()
                 .AsNoTracking()
-                .FirstOrDefaultAsync(course => course.Code == request.Code, cancellationToken);
-            if (course == null)
+                .FirstOrDefaultAsync(instance => instance.Course.Code == request.Code, cancellationToken);
+            if (courseInstance == null)
             {
                 return Result<CourseResponse>.Failure("Course not found");
             }
             var courseResponse = new CourseResponse
             {
-                CourseId = course.CourseId,
-                Name = course.Name,
-                Code = course.Code,
-                Category = course.Category,
-                Credit = course.Credit,
-                Description = course.Description
+                CourseId = courseInstance.Course.CourseId,
+                Name = courseInstance.Course.Name,
+                Code = courseInstance.Course.Code,
+                Category = courseInstance.Course.Category,
+                Credit = courseInstance.Course.Credit,
+                Description = courseInstance.Course.Description,
+                InstanceDescription = courseInstance.Description,
+                StartDate = courseInstance.Semester.StartDate,
+                EndDate = courseInstance.Semester.EndDate,
             };
             return Result<CourseResponse>.Ok(courseResponse);
         }
--- anyu-ui/src/app/models/course.ts
+++ anyu-ui/src/app/models/course.ts
@@ -1,10 +1,13 @@
 export class Course {
-  id: number;
+  courseId: number;
   name: string;
   code: string;
   category: string;
   credit: number;
   description: string;
+  instanceDescription: string;
+  startDate: Date;
+  endDate: Date;
 
   // constructor(name: string) {
   //   this.name = name;
